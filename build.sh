5d297832c867a41af0fb412806bb08806cf7da54
Brightshine <brightshine.hsieh@gmail.com>
2015-10-21 17:50:30 +0800

    Remove unnecessary 'cd'

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 readonly BUILDER='FDOEMCD.builder.zip'
 readonly ISOLINUX_CFG='FDOEMCD/CDROOT/isolinux/ISOLINUX.CFG'
 readonly AUTORUN='FDOEMCD/CDROOT/isolinux/AUTORUN.BAT'
+readonly TOPDIR='FDOEMCD'
 
 [ ! -f ] && wget -N http://www.fdos.info/bootdisks/ISO/${BUILDER}
-[ -d ] && rm -rf FDOEMCD
+[ -d ] && rm -rf ${TOPDIR}
 unzip ${BUILDER}
 sed -i '\/prompt 1/d' ${ISOLINUX_CFG}
 sed -i '\/display BOOTMSG/d' ${ISOLINUX_CFG}
 sed -i '\/timeout /d' ${ISOLINUX_CFG}
 
-cd FDOEMCD
-mkisofs -o FreeDOS.iso -b isolinux/isolinux.bin -input-charset utf8 -no-emul-boot -boot-load-size 4 -boot-info-table -N -J -r -c boot.catalog -hide boot.catalog -hide-joliet boot.catalog CDROOT
+mkisofs -o FreeDOS.iso -b isolinux/isolinux.bin -input-charset utf8 -no-emul-boot -boot-load-size 4 -boot-info-table -N -J -r -c boot.catalog -hide boot.catalog -hide-joliet boot.catalog ${TOPDIR}/CDROOT
 
