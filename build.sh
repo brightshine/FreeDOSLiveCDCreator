853fd919effacfe4385460e3609079d374098830
Brightshine <brightshine.hsieh@gmail.com>
2015-10-21 16:53:49 +0800

    Trim prompt message & skip timeout

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 readonly BUILDER='FDOEMCD.builder.zip'
+readonly ISOLINUX_CFG='FDOEMCD/CDROOT/isolinux/ISOLINUX.CFG'
+readonly AUTORUN='FDOEMCD/CDROOT/isolinux/AUTORUN.BAT'
 
 [ ! -f ] && wget -N http://www.fdos.info/bootdisks/ISO/${BUILDER}
 [ -d ] && rm -rf FDOEMCD
 unzip ${BUILDER}
+sed -i '\/prompt 1/d' ${ISOLINUX_CFG}
+sed -i '\/display BOOTMSG/d' ${ISOLINUX_CFG}
+sed -i '\/timeout /d' ${ISOLINUX_CFG}
 
 cd FDOEMCD
-mkisofs -o testdisk.iso -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -N -J -r -c boot.catalog -hide boot.catalog -hide-joliet boot.catalog CDROOT
+mkisofs -o FreeDOS.iso -b isolinux/isolinux.bin -input-charset utf8 -no-emul-boot -boot-load-size 4 -boot-info-table -N -J -r -c boot.catalog -hide boot.catalog -hide-joliet boot.catalog CDROOT
 
