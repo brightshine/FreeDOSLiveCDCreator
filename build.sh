7f3e058ad0e92fc7b6983853b846b1ddaf821efc
Brightshine <brightshine.hsieh@gmail.com>
2015-10-22 16:16:58 +0800

    Refactor script

--- build.sh
+++ build.sh
@@ -3,13 +3,17 @@ readonly BUILDER='FDOEMCD.builder.zip'
 readonly ISOLINUX_CFG='FDOEMCD/CDROOT/isolinux/ISOLINUX.CFG'
 readonly AUTORUN='FDOEMCD/CDROOT/isolinux/AUTORUN.BAT'
 readonly TOPDIR='FDOEMCD'
+readonly ISO_FS_OPT='-input-charset utf8 -no-emul-boot -boot-load-size 4 -boot-info-table -N -J -r -c boot.catalog -hide boot.catalog -hide-joliet boot.catalog'
 
 [ ! -f ] && wget -N http://www.fdos.info/bootdisks/ISO/${BUILDER}
+
 [ -d ] && rm -rf ${TOPDIR}
 unzip ${BUILDER}
+
+# Remove unnecessary prompt & timeout
 sed -i '\/prompt 1/d' ${ISOLINUX_CFG}
 sed -i '\/display BOOTMSG/d' ${ISOLINUX_CFG}
 sed -i '\/timeout /d' ${ISOLINUX_CFG}
 
-mkisofs -o FreeDOS.iso -b isolinux/isolinux.bin -input-charset utf8 -no-emul-boot -boot-load-size 4 -boot-info-table -N -J -r -c boot.catalog -hide boot.catalog -hide-joliet boot.catalog ${TOPDIR}/CDROOT
+mkisofs -o FreeDOS.iso -b isolinux/isolinux.bin ${ISO_FS_OPT} ${TOPDIR}/CDROOT
 
